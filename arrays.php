ee0e512b2aec3c5b05ab0ad5f3a403116fa17c85
Sultonmurod111 <sultonmurodkomiljonov200@gmail.com>
2025-11-01 16:33:57 +0500

    test

--- arrays.php
+++ arrays.php
@@ -1,6 +1,12 @@
 <?php
+// Birinchi usul 
+$sonlar = [1,5,4,2,5,8];
+// Ikkinchi usul
+$sonlar1 = array(2,6,8,9,6,5,8);
 
-
+$son = [23,5,6,7];
+$cars = ['Cobalt', 'Damas','Malibu','Onix'];
+echo $son[0];
 
 
 ?>
