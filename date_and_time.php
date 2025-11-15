4e0b5649e3242cdf362be23ec04855371c2cce89
Sultonmurod111 <sultonmurodkomiljonov200@gmail.com>
2025-11-15 15:34:32 +0500

    Izoh qoldirdim

---
+++ date_and_time.php
@@ -0,0 +1,11 @@
+<?php
+// 15.11.2025
+echo strtotime("+7 days")."<br>";
+echo date("d.m.Y H:i", strtotime("-10 days"))."<br>";
+echo date("Y-m-d",strtotime("last Thursday"))."<br>";
+echo date("Y-m-d",strtotime("Next 2 yrs")). "<br>";
+echo date("Y-m-d",strtotime("2007.08.17")). "<br>";
+$t_yil = "2011.12.05";
+$yosh = (time()- strtotime($t_yil)) / (60 * 60 * 24 * 365);
+echo (int)$yosh;
+?>
--- superglobals.php
+++ superglobals.php
@@ -2,4 +2,17 @@
 var_dump($_SERVER);
 echo $_SERVER['REQUEST_METHOD']."<br>";
 echo $_SERVER['PHP_SELF']."<br>";
+
+
+
+$x = 75;
+  
+function myfunction() {
+  echo $GLOBALS['x']."<br>";
+}
+
+myfunction()
+
+
+
 ?>
